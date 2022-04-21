3f0f8dfa46ea96cdd3da7bed20b6e62c410e8839
mcnuggetsx20 <alienbaxy@gmail.com>
2022-04-21 23:38:08 +0200

    21 Apr 2022 23:38:08

--- walczyn_autorskie/pillow/meta.py
+++ walczyn_autorskie/pillow/meta.py
@@ -1,12 +1,14 @@
 from exif import Image
 from math import sin, cos, acos, radians, sqrt
 
-def l(a, b, p):
-    a = radians(a)
-    b = radians(b)
-    p = radians(p)
-    return acos(cos(a*p) * cos(b*p) + sin(a*p) * sin(b*p) * cos(p)) * 111.1
-    
+def l(a, b):
+    a = [i for i in a]
+    b = [i for i in b]
+    a[0] = radians(a[0])
+    a[1] = radians(a[1])
+    b[0] = radians(b[0])
+    b[1] = radians(b[1])
+    return acos((sin(a[0]) * sin(b[0])) + (cos(a[0]) * cos(b[0]) * cos(abs(a[1]-b[1])))) * 111.1
 
 def link_gen(lat, lon):
     link = 'https://www.google.pl/maps/place/' + lat + ',' + lon
@@ -21,18 +23,16 @@ def func(img, pin, radius):
     lat = lat[0] + lat[1]/60 +lat[2]/3600
     lon = lon[0] + lon[1]/60 +lon[2]/3600
 
+    dst = l((lat, lon), pin)
 
-    x = l(pin[0], lat, 90)
-    y = l(pin[1], lon, 90)
-
-    dst = sqrt(x**2 + y**2)
     if dst <= radius:
+        print(dst)
         return link_gen(str(lat), str(lon))
     else:
         return None
 
 wro = (51.110048, 17.060480)
-print(func('alo.jpg', wro, 200))
+print(func('alo.jpg', wro, 100))
 
 
 
