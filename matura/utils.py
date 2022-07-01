b806f7b47e37bbbf2847823f06e33c9e08a646fa
mcnuggetsx20 <alienbaxy@gmail.com>
2022-07-01 22:47:43 +0200

    01 Jul 2022 22:47:43

---
+++ matura/utils.py
@@ -0,0 +1,22 @@
+class line:
+    def __init__(self, headers, rows):
+        for i, j in enumerate(headers):
+            setattr(self, j, rows[i])
+
+class Table(list):
+    def __init__(self, file):
+        table = open(file, 'r').read().split('\n')[:-1]
+        self.headers = table[0].split('\t')
+        table = table[1:]
+        for i in table:
+            row = i.split('\t')
+            self.append(line(self.headers, row))
+
+    def find(self, key=None):
+        res = []
+        for i in self:
+            if key in i.__dict__.values():
+                res.append(i)
+        return res
+
+
