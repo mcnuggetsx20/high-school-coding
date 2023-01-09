4799bca69926f1fd027f32791ee45d7334746f0c
mcnuggetsx20 <alienbaxy@gmail.com>
2023-01-09 16:53:01 +0100

    09 Jan 2023 16:53:00

--- matura/2022-probna/z3.py
+++ matura/2022-probna/z3.py
@@ -17,6 +17,7 @@ tab = [i for i in range(len(sieve)) if sieve[i] ]
 file = open('dane/liczby.txt', 'r').read().split()
 file = [ int(i) for i in file]
 
+#3.3
 from collections import defaultdict as dd
 mp = dd(lambda: 0)
 
@@ -31,4 +32,18 @@ mp =  sorted(mp.items(), key=lambda x:x[1])[::-1]
 print(*mp[0])
 print(*mp[-1])
 
+#3.4
+print()
 
+ans=  dd(lambda:0)
+for i in file:
+    a = hex(i)[2:]
+    for j in a:
+        ans[j] +=1
+
+for i in range(10):
+    j = str(i)
+    print(j, ans[j])
+
+for i in range(6):
+    print(chr(65 + i), ans[ chr(97+i) ])
