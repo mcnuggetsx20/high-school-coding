04fbc35426abc28c5f5a8429ea16a7a9bebbe0cd
mcnuggetsx20 <alienbaxy@gmail.com>
2023-02-20 21:24:30 +0100

    20 Feb 2023 21:24:27

--- matura/2021_marzec/z6.py
+++ matura/2021_marzec/z6.py
@@ -1,120 +1,115 @@
-from collections import defaultdict as dd
-from datetime import datetime
+def parse(name):
+    tab = open(f'dane/{name}.txt', 'r').read().split('\n')[:-1]
+    tab = [dict(zip(tab[0].split('\t'), i.split('\t'))) for i in tab[1:]]
+    globals()[name] = tab
+    return
 
-grupy = open('dane/grupy.txt', 'r').read().split('\n')[:-1]
-zawodnicy = open('dane/zawodnicy.txt', 'r').read().split('\n')[:-1]
-startujacy = open('dane/startujacy.txt', 'r').read().split('\n')[:-1]
-czasy = open('dane/czasy.txt', 'r').read().split('\n')[:-1]
-
-grupy = [dict(zip( grupy[0].split(), i.split('\t'))) for i in grupy[1:]]
-zawodnicy = [dict(zip( zawodnicy[0].split(), i.split('\t'))) for i in zawodnicy[1:]]
-startujacy = [dict(zip( startujacy[0].split(), i.split('\t'))) for i in startujacy[1:]]
-czasy = [dict(zip( czasy[0].split(), i.split('\t'))) for i in czasy[1:]]
+parse('grupy')
+parse('czasy')
+parse('startujacy')
+parse('zawodnicy')
 
 #6.1
-imiona = {i['id_zawodnika']: i['imie'] + ' ' + i['nazwisko'] for i in zawodnicy}
-starty = {i['id_startu']: imiona[ i['id_zawodnika']] for i in startujacy}
+
+imiona1 = {i['id_zawodnika']:i['imie'] + ' ' + i['nazwisko'] for i in zawodnicy}
+imiona = {i['id_startu']: imiona1[i['id_zawodnika']] for i in startujacy}
 
 from collections import defaultdict as dd
-from datetime import datetime, timedelta
-ans = dd(lambda:10000000000000000000000000000000)
+ans = dd(lambda:10**10)
+
 for i in czasy:
-    a = list(map(int, i['czas'].split(':')))
-    czas = a[0]*60000 + a[1] * 1000 + a[2]
+    ind = i['id_startu']
+    czas = i['czas'].split(':')
+    czas = 60000 * int(czas[0]) + 1000*int(czas[1]) + int(czas[-1])
+    imie = imiona[ind]
+    ans[imie] = min(ans[imie], czas)
+ans = sorted(ans.items(), key=lambda x:x[1])
+print(*ans[0])
 
-    ind = starty[ i['id_startu']]
-    ans[ind] = min(ans[ind], czas)
-
-ans = sorted(ans.items(), key=lambda x:x[1])[0]
-print(ans[0], end =' ')
-temp = ans[1]
-
-print(temp//60000,end=':')
-temp %=60000
-print(temp//1000, end=':')
-temp %=1000
-print(temp)
-
+ans = dd(lambda:0)
 #6.2
-print()
-ans = dd(lambda:set())
 for i in startujacy:
-    if i['obywatel_kraju'] != 'Polska': continue
-
     ind = i['id_zawodnika']
-    ind = imiona[ind]
+    kraj = i['obywatel_kraju']
+    if kraj != 'Polska': continue
+    ans[ind] += 1
 
-    ans[ind].add(i['rok'])
-ans = {i:len(ans[i]) for i in ans}
 ans = sorted(ans.items(), key=lambda x:x[1])[-1]
-print(*ans)
+print(imiona1[ans[0]], ans[1])
 
 #6.3
 print()
-ans = dd(lambda: [0,[]])
-lata = {i['id_zawodnika']: i['data'] for i in zawodnicy}
+wiek = {i['id_zawodnika']: int(i['data'].split('-')[0]) for i in zawodnicy}
+
+ans = dd(lambda:[])
 
 for i in startujacy:
-    ind = i['id_zawodnika']
-    rok = i['rok']
-    ur = int(lata[ind].split('-')[0])
+    rok = int(i['rok'])
+    zaw = i['id_zawodnika']
+    age = rok - wiek[zaw]
 
-    if ur > ans[rok][0]:
-        ans[rok][0] = ur
-        ans[rok][1] = []
-        ans[rok][1].append( imiona[ind])
-
-    elif ur == ans[rok][0]:
-        ans[rok][1].append( imiona[ind])
-
+    ans[rok].append( (age, zaw))
 for i in ans:
-    print(i, ','.join(ans[i][1]))
+    print(i)
+    ans[i].sort()
+    for j in ans[i]:
+        if j[0] != ans[i][0][0]: continue
+        print(imiona1[j[1]])
+    print()
 
 #6.4
 print()
-c = dd(lambda: False)
-for i in czasy: c[ i['id_startu']] = True
-
+c = [i['id_startu'] for i in czasy]
 ans = dd(lambda:0)
-for i in startujacy:
-    if c[i['id_startu']]: continue
 
-    ans[ i['rok']] += 1
-ans = sorted(ans.items(), key=lambda x:x[1])[-1]
-print(*ans)
+for i in startujacy:
+    ind = i['id_startu']
+    rok = i['rok']
+    if ind in c: continue
+    ans[rok] += 1
+
+ans = sorted(ans.items(), key=lambda x:x[1])
+print(*ans[-1])
 
 #6.5
 print()
-ans = dd(lambda: dd(lambda: set()))
-nazwy = {i['kod_grupy']: i['nazwa'] for i in grupy}
+g = {i['kod_grupy']: i['nazwa'] for i in grupy}
 
+ans = dd(lambda: dd(lambda:set()))
+ans2 = dd(lambda: [])
 for i in startujacy:
+    gr= i['kod_grupy']
     rok = i['rok']
-    ind = i['kod_grupy']
+    kraj = i['obywatel_kraju']
 
-    ans[rok][ind].add(i ['obywatel_kraju'])
+    ans[rok][gr].add(kraj)
 for i in ans:
     for j in ans[i]:
-        ans[i][j] = len(ans[i][j])
-    ans[i] = [j for j in ans[i] if ans[i][j]==1]
-for i in ans:
-    print(i, len(ans[i]))
+        if len(ans[i][j]) != 1: continue
+        ans2[i].append(g[j])
 
-ans = sorted(ans.items(), key=lambda x:len(x[1]))[-1]
-temp = ','.join([nazwy[i] for i in ans[1]])
-print(ans[0], temp)
+ans = sorted(ans2.items(), key=lambda x: x[0])
+
+for i in ans:
+    print(i[0], len(i[1]))
+
+ans2 = sorted(ans2.items(), key=lambda x: len(x[1]))
+print(*ans2[-1])
 
 #6.6
 print()
-ans = dd(lambda:set())
+
+ans = dd(lambda: set())
+
 for i in startujacy:
     ind = i['id_zawodnika']
-    ind = imiona[ind]
-
-    ans[ind].add(i['obywatel_kraju'])
+    kraj = i['obywatel_kraju']
+    ans[ind].add(kraj)
 
 for i in ans:
-    if len(ans[i]) == 1: continue
+    if len(ans[i])==1: continue
+    print(f'{imiona1[i]}: {ans[i]}')
 
-    print(i, '-', ','.join(ans[i]))
+
+
 
